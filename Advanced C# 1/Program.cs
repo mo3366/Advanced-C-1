e91776caa0b6c492e210e33f7d4aae9e4ac9f278
mo3366 <momen23012694@gmail.com>
2026-09-13 06:34:31 +0300

    Q2: Write a generic class Container<T> with Add and Get methods.

--- Advanced C# 1/Program.cs
+++ Advanced C# 1/Program.cs
@@ -1,18 +1,27 @@
-﻿namespace Advanced_C__1
+﻿using System.Diagnostics.Metrics;
+using Advanced_C__1.generic_class;
+namespace Advanced_C__1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Q1: What is a generic class? Why use generics?
-            
+
             //generic class : A generic class allows you to write classes with type parameters where the actual type is specified when the code is used rather than when it is written.
-            
+
             //Why use generics -->
             //type Safety: Catches type mismatches at compile time instead of runtime
             //Reusability: One implementation works for many different types
             //Better Performance: Avoids boxing and unboxing overhead for value types
             //Cleaner Code: Reduces code duplication and eliminates the need for separate classes
+            #endregion
+
+            #region Q2: Write a generic class Container<T> with Add and Get methods.
+            Container<int> container = new Container<int>();
+            container.Add(10);
+            container.Add(20);
+            Console.WriteLine(container.Get(0));
             #endregion
         }
     }
---
+++ Advanced C# 1/generic class/Container.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advanced_C__1.generic_class
+{
+    internal class Container<T>
+    {
+        private List<T> _items = new List<T>();
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+        public T Get(int index)
+        {
+            return _items[index];
+        }
+    }
+}
