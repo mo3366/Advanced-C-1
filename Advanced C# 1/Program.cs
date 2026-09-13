cb694944547c56c07508d6c7bfb3b4773a21b9e7
mo3366 <momen23012694@gmail.com>
2026-09-13 06:14:01 +0300

    Q1: What is a generic class? Why use generics?

--- Advanced C# 1/Program.cs
+++ Advanced C# 1/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1: What is a generic class? Why use generics?
+            
+            //generic class : A generic class allows you to write classes with type parameters where the actual type is specified when the code is used rather than when it is written.
+            
+            //Why use generics -->
+            //type Safety: Catches type mismatches at compile time instead of runtime
+            //Reusability: One implementation works for many different types
+            //Better Performance: Avoids boxing and unboxing overhead for value types
+            //Cleaner Code: Reduces code duplication and eliminates the need for separate classes
+            #endregion
         }
     }
 }
