6a686e606f49e14b5083cfe8493f3e82f09cb192
mo3366 <momen23012694@gmail.com>
2026-09-13 09:07:21 +0300

    Q14: Write a SafeList<T> that returns default when the index is invalid.

--- Advanced C# 1/Program.cs
+++ Advanced C# 1/Program.cs
@@ -185,8 +185,27 @@ namespace Advanced_C__1
 
             #endregion
 
+            #region Q14: Write a SafeList<T> that returns default when the index is invalid.
+            //public class SafeList<T>
+            //{
+            //private List<T> _items = new List<T>();
 
-        }
+            //public void Add(T item) => _items.Add(item);
+
+            //public T Get(int index)
+            //{
+            //    if (index >= 0 && index < _items.Count)
+            //    {
+            //        return _items[index];
+            //    }
+            //    return default(T);
+            //}
+            //}
+
+            #endregion
+
+
+    }
 
     }
 }
